572aee420be65112852920dd28fc592a03acd87d
Filipe Morais <afilipem@gmail.com>
2022-03-15 19:08:38 -0500

    adds the code line for using Microsoft Edge

--- src/introductionselenium/SelIntroduction.java
+++ src/introductionselenium/SelIntroduction.java
@@ -1,21 +1,36 @@
 package introductionselenium;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SelIntroduction {
 
 	public static void main(String[] args) {
 		System.out.println( "Hello World!" );
 
-        // Setting the chromedriver path
-        System.setProperty("webdriver.chrome.driver", "C:\\1.installers\\chromedriver_win32\\chromedriver.exe");
-
-        //Invoking browser
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://rahulshettyacademy.com/#/index");
-        System.out.println(driver.getTitle());
-        //driver.close();
-        driver.quit(); //it is used to quit all the browser pages
+//        // Setting the chromedriver path
+//        System.setProperty("webdriver.chrome.driver", "C:\\1.installers\\chromedriver_win32\\chromedriver.exe");
+//
+//        //Invoking browser
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://rahulshettyacademy.com/#/index");
+//        System.out.println(driver.getTitle());
+//        //driver.close();
+//        driver.quit(); //it is used to quit all the browser pages
+		
+		//System.setProperty("webdriver.gecko.driver", "C:\\1.installers\\geckodriver-v0.30.0-win64\\geckodriver.exe");
+		//WebDriver driver = new FirefoxDriver();
+		
+		System.setProperty("webdriver.edge.driver", "C:\\1.installers\\\\edgedriver_win64\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		
+		//Invoking browser
+		driver.get("https://rahulshettyacademy.com/#/index");
+		System.out.println(driver.getTitle());
+		//driver.close();
+		driver.quit(); //it is used to quit all the browser pages
+		
         }
 
 }
