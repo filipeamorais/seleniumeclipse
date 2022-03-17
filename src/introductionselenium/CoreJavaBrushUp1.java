44ade400209fb0eedabf473cf230d94274ab6bd0
Filipe Morais <afilipem@gmail.com>
2022-03-16 22:57:13 -0500

    adds class for java exploration

---
+++ src/introductionselenium/CoreJavaBrushUp1.java
@@ -0,0 +1,16 @@
+package introductionselenium;
+
+public class CoreJavaBrushUp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myNum = 5;
+		String website = "example 1";
+		char letter = 'e';
+		double dec = 5.99;
+		boolean myCard = true;
+		
+		System.out.println(myNum+"example 2");
+	}
+
+}
