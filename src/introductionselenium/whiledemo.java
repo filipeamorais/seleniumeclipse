6a29a6dd45e4c2f14c879618222d7f4c63b916f1
Filipe Morais <afilipem@gmail.com>
2022-03-17 13:59:34 -0500

    adds class for while loop

---
+++ src/introductionselenium/whiledemo.java
@@ -0,0 +1,28 @@
+package introductionselenium;
+
+public class whiledemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//while loop
+		//1 to 10
+		int i = 10;
+		
+//		while(i>0) {
+//			System.out.println(i);
+//			i--;
+//			
+//		}
+		int j=20;
+		
+		do { //first it executes and then it check, 1 loop is guaranteed
+			
+		j++;
+		}while(j<30);
+		
+		System.out.println(j);
+
+	}
+
+}
