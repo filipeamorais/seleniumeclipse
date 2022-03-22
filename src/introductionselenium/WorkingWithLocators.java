7fa6a312492f42e999a48529f115d9b814467ae2
Filipe Morais <afilipem@gmail.com>
2022-03-21 22:52:12 -0500

    fix the code for finding locator

--- src/introductionselenium/WorkingWithLocators.java
+++ src/introductionselenium/WorkingWithLocators.java
@@ -2,10 +2,11 @@ package introductionselenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class WorkingWithLocators {
 
-		public static void main(String[] args) {
+		public static void main(String[] args) throws InterruptedException {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\1.installers\\chromedriver_win32\\chromedriver.exe");
 
@@ -15,12 +16,43 @@ public class WorkingWithLocators {
 
 		//header/div/button[1]/following-sibling::button[1]
 
-		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
-		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+//		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+//
+//
+//
+//		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText());
+		driver.findElement(By.id("divpaxinfo")).click();
 
+		   Thread.sleep(2000L);
 
+		   /*int i=1;
 
-		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText());
+		while(i<5)
+
+		{
+
+		driver.findElement(By.id("hrefIncAdt")).click();//4 times
+
+		i++;
+
+		}*/
+
+		   System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+
+		for(int i=1;i<5;i++)
+
+		{
+
+		driver.findElement(By.id("hrefIncAdt")).click();
+
+		}
+
+		driver.findElement(By.id("btnclosepaxoption")).click();
+
+		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
+
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
 		}
 }
